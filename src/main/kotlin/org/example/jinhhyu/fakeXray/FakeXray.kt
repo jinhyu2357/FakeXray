cb6647c5b2f52bb1a23d858282fdd00a943e08a7
jinhyu2357 <jinhyu2357@gmail.com>
2026-03-02 20:57:30 +0900

    Initial commit

---
+++ src/main/kotlin/org/example/jinhhyu/fakeXray/FakeXray.kt
@@ -0,0 +1,14 @@
+package org.example.jinhhyu.fakeXray
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class FakeXray : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
